2ecde945dd9b388421340eb271cf00dfe4fac2ff
ka.andy <k.andriele@escolar.ifrn.edu.br>
2026-06-04 18:41:56 -0300

    trocando o for por stream
    O metodo de contagem de livros dispoveis foi trocado o laco de repeticao por stream do proprio java.

--- src/main/java/br/edu/ifrn/qagym/service/LibraryService.java
+++ src/main/java/br/edu/ifrn/qagym/service/LibraryService.java
@@ -56,17 +56,12 @@ public class LibraryService {
         return 0;
     }
 
-    public int countAvailableBooks() {
-        int n = 0;
-
-    for (Book book : books) {
-        if (book.isAvailable()) {
-            n++;
-        }
-    }
-    return n;
-        // TODO: implementar contagem de livros disponíveis
-    }
+    
+    public int countAvaliableBooks() {
+    return (int) books.stream()
+                      .filter(Book::isAvailable)
+                      .count();
+}
 
     public int countUnavailableBooks() {
         // TODO: implementar contagem de livros indisponíveis
